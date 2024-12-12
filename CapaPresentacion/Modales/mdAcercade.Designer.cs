f549a38a58c9d7beef46bbe5a4e63310bc5721d9
Roldanuriel <uriroldan06@gmail.com>
2024-12-11 19:18:26 -0600

    se agrego categoria

--- CapaPresentacion/Modales/mdAcercade.Designer.cs
+++ CapaPresentacion/Modales/mdAcercade.Designer.cs
@@ -40,28 +40,29 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(105, 304);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(176, 13);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 36;
-            this.label3.Text = "Desarrollado por: Codigo Estudiante";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(15, 89);
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 95);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(269, 174);
+            this.label4.Size = new System.Drawing.Size(269, 193);
             this.label4.TabIndex = 34;
             this.label4.Text = resources.GetString("label4.Text");
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(269, 35);
             this.label2.TabIndex = 35;
-            this.label2.Text = "Este sistema se publica bajo los términos de la licenca MIT";
+            this.label2.Text = "Fundamentos de Bases De Datos";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label1
@@ -70,9 +71,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(11, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.Size = new System.Drawing.Size(275, 20);
             this.label1.TabIndex = 33;
-            this.label1.Text = "Open Source";
+            this.label1.Text = "Instituto Tecnologico De Tlaxiaco";
             // 
             // mdAcercade
             // 
--- CapaPresentacion/Modales/mdAcercade.cs
+++ CapaPresentacion/Modales/mdAcercade.cs
@@ -16,5 +16,15 @@ namespace CapaPresentacion.Modales
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
